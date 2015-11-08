c8dadcc772c994d13a55a19b2246701cfcf55833
Ryan Gray <ryan.is.gray@gmail.com>
2015-11-08 14:02:54 -0600

    Some restructure for sake of templating. Templating user input into generated project.

--- app/index.js
+++ app/index.js
@@ -6,6 +6,46 @@ var chalk = require('chalk');
 var generators = require('yeoman-generator');
 
 var jsWorkbenchGenerator = generators.Base.extend({
+  promptUser: function() {
+    var done = this.async();
+    var prompts = [
+          {
+            name: 'projectName',
+            message: 'What is your project\'s name ?',
+            default: 'JS-Workbench-Project'
+          },
+          {
+            name: 'description',
+            message: 'Describe your project'
+          },
+          {
+            name: 'version',
+            message: 'Version',
+            default: '0.0.1'
+          },
+          {
+            name: 'author',
+            message: 'Name of project author'
+          },
+          {
+            name: 'license',
+            message: 'License',
+            default: 'WTFPL'
+          },
+          {
+            name: 'homepage',
+            message: 'Project homepage'
+          }
+        ];
+
+    // After prompt occurs, an object 'props' is passed to callback with user responses
+    this.prompt(prompts, function (props) {
+      this.npmProps = props;
+      this.mainJS = this.npmProps.projectName + '.js';
+      done();
+    }.bind(this));
+  },
+
   // Creating folders that won't be created during file copy
   scaffoldFolders: function() {
     var dirPaths = ['dist', 'dev/tests'];
@@ -15,27 +55,35 @@ var jsWorkbenchGenerator = generators.Base.extend({
       mkdirp(pathToDir, dirMade.bind(this, pathToDir));
     }
 
-    // Remember everyone, this function gets hoisted
+    // Remember folks, this function gets hoisted
     function dirMade (pathToDir, err) {
-      if (err) {
-        console.log(chalk.red(err));
-      } else {
-        console.log(chalk.green('mkdir ') + pathToDir);
-      }
+      var msg = err ? chalk.red(err) : chalk.green('mkdir ') + pathToDir;
+      console.log(msg);
     }
   },
 
   // Copying core files and stubs from generator source to new project
   // Very stringy... better approach?
   copyFiles: function () {
-    this.copy('../_Gruntfile.js', 'Gruntfile.js');
-    this.copy('../_jscs.json', 'jscs.json');
-    this.copy('../_package.json', 'package.json');
-    this.copy('../._gitignore', '.gitignore');
-    this.copy('../dev/js/main.js', 'dev/js/main.js');
-    this.copy('../dev/js/exampleDependency.js', 'dev/js/exampleDependency.js');
-    this.copy('../dev/css/sass/general.scss', 'dev/css/sass/general.scss');
-    this.copy('../dev/css/sass/particles/_general.scss', 'dev/css/sass/particles/_general.scss');
+    this.copy('_Gruntfile.js', 'Gruntfile.js');
+    this.copy('_jscs.json', 'jscs.json');
+    this.copy('._gitignore', '.gitignore');
+    this.copy('dev/js/main.js', 'dev/js/' + this.mainJS);
+    this.copy('dev/js/exampleDependency.js', 'dev/js/exampleDependency.js');
+    this.copy('dev/css/sass/general.scss', 'dev/css/sass/general.scss');
+    this.copy('dev/css/sass/particles/_general.scss', 'dev/css/sass/particles/_general.scss');
+
+    this.fs.copyTpl(
+      this.templatePath('_package.json'),
+      this.destinationPath('package.json'),
+      this.npmProps
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('_Gruntfile.js'),
+      this.destinationPath('Gruntfile.js'),
+      { mainJS: this.mainJS }
+    )
   },
 
   // Install Node Packages from copied package.json, then show thank you message
