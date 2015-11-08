737f5cc66a02d95064f8a9e41378a8c38e8bba76
Ryan Gray <ryan.is.gray@gmail.com>
2015-11-08 16:38:25 -0600

    Making stub example html. Handling some setup stuff synchronously. Preserving original bundle plus copying, renaming, and minifying.

---
+++ app/execSync.js
@@ -0,0 +1,23 @@
+var child_process = require('child_process');
+var fs = require('fs');
+
+function execSync(command) {
+  // Run the command in a subshell
+  child_process.exec(command + ' 2>&1 1>output && echo done! > done');
+
+  // Block the event loop until the command has executed.
+  while (!fs.existsSync('done')) {
+  // Do nothing
+  }
+
+  // Read the output
+  var output = fs.readFileSync('output');
+
+  // Delete temporary files.
+  fs.unlinkSync('output');
+  fs.unlinkSync('done');
+
+  return output;
+}
+
+module.exports = execSync;
